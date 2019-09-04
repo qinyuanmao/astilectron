0301188c5bcd0f87e824bf4b65c54db2be288131
大黄 <chemwenxin@163.com>
2019-09-04 15:43:39 +0800

    mac系统在点应用时,隐藏的窗口展示

--- main.js
+++ main.js
@@ -45,6 +45,14 @@ for (let i = idx; i < process.argv.length; i++) {
 
 // App is quitting
 app.on('before-quit', () => quittingApp = true);
+// mac 当应用被激活时发出。 各种操作都可以触发此事件, 例如首次启动应用程序、尝试在应用程序已运行时或单击应用程序的坞站或任务栏图标时重新激活它。
+app.on('activate', () => {
+    for (let id in elements) {
+        if (elements[id].isVisible && (elements[id].isVisible() === false)) {
+            elements[id].show()
+        }
+    }
+});
 
 // App is ready
 app.on('ready', () => {
